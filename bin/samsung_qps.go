ff19d12c55c9e7b2bda911edb70091ee2913439d
Xintao <hunterlxt@live.com>
2020-06-22 17:02:05 +0800

    init qps worker program
    Signed-off-by: Xintao <hunterlxt@live.com>

---
+++ bin/samsung_qps.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+	"math/rand"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	ip      = flag.String("ip", "127.0.0.1", "ip")
+	port    = flag.Int("port", 10000, "port")
+	db_name = flag.String("db", "test", "db")
+)
+
+var (
+	sql1 = `create table if not exists t (
+		id int not null,
+		c1 char(64),
+		c2 char(64),
+		c3 char(64),
+		c4 char(64),
+		c5 char(64)
+	)
+	PARTITION BY RANGE (id) (
+		PARTITION p0 VALUES LESS THAN (10000),
+		PARTITION p1 VALUES LESS THAN (20000),
+		PARTITION p2 VALUES LESS THAN (30000),
+		PARTITION p3 VALUES LESS THAN (40000),
+		PARTITION p4 VALUES LESS THAN MAXVALUE
+	)`
+	sql2 = `insert into t values`
+	sql3 = `ALTER TABLE t DROP PARTITION `
+)
+
+func main() {
+	flag.Parse()
+
+	db := connect(*ip, *port, *db_name)
+	create_table(db)
+	insert_data(db)
+
+	fmt.Println("1. Which partition do you want to drop? [p0~p4]:")
+	var name string
+	fmt.Scan(&name)
+	drop_partition(db, name)
+	fmt.Println("2. Which partition do you want to drop? [p0~p4]:")
+	fmt.Scan(&name)
+	drop_partition(db, name)
+	fmt.Println("Enter to exit")
+	fmt.Scanln()
+}
+
+func connect(ip string, port int, db string) *sql.DB {
+	dsn := fmt.Sprintf("root@tcp(%s:%d)/%s", ip, port, db)
+	fmt.Println("connecting", dsn)
+	dbConn, err := sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return dbConn
+}
+
+func create_table(db *sql.DB) {
+	rand.Seed(time.Now().Unix())
+	_, err := db.Exec(sql1)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func drop_partition(db *sql.DB, name string) {
+	sql := sql3 + name
+	_, err := db.Exec(sql)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func insert_data(db *sql.DB) {
+	insert_data_job(db, 8, 2, 0)
+	insert_data_job(db, 8, 2, 1)
+	insert_data_job(db, 8, 2, 2)
+	insert_data_job(db, 8, 2, 3)
+	insert_data_job(db, 8, 2, 4)
+	fmt.Println("All insert job started...")
+}
+
+func insert_data_job(db *sql.DB, concurrent int, batch int, part_num int) {
+	for i := 0; i < concurrent; i++ {
+		conn, err := db.Conn(context.Background())
+		if err != nil {
+			fmt.Println(err)
+		}
+		go func() {
+			id := part_num*10000 + rand.Intn(10000)
+			str := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+"
+			insert_sql := sql2
+			for i := 0; i < batch; i++ {
+				if i == batch-1 {
+					insert_sql += fmt.Sprintf(" (%v, '%v', '%v', '%v', '%v', '%v')", id+i, str, str, str, str, str)
+					break
+				} else {
+					insert_sql += fmt.Sprintf(" (%v, '%v', '%v', '%v', '%v', '%v'),", id+i, str, str, str, str, str)
+				}
+			}
+			ret, err := conn.ExecContext(context.Background(), insert_sql)
+			if err != nil {
+				fmt.Println(err)
+			}
+			ret.RowsAffected()
+		}()
+	}
+}
