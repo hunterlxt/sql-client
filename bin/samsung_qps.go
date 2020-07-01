a32d34f35b6ff07da7609adcde873d40ac66ddc6
Xintao <hunterlxt@live.com>
2020-07-01 17:59:51 +0800

    update
    Signed-off-by: Xintao <hunterlxt@live.com>

--- bin/samsung_qps.go
+++ bin/samsung_qps.go
@@ -12,17 +12,17 @@ import (
 )
 
 var (
-	ip            = flag.String("ip", "127.0.0.1", "ip")
-	port          = flag.Int("port", 10000, "port")
-	db_name       = flag.String("db", "test", "db")
-	concurrent    = flag.Int("concurrent", 16, "concurrent for insert")
-	batch         = flag.Int("batch", 64, "batch for insert")
-	shared_flag   = [4]bool{false}
-	timer         = time.NewTimer(1 * time.Second)
-	enable_insert = flag.Bool("insert", false, "enable_insert")
-	insert_time   = flag.Int("insert_time", 6, "insert_time hour")
-	drop_test     = flag.Bool("drop_test", true, "drop_test")
-	drop_delay    = flag.Int("drop_delay", 45, "drop_delay")
+	ip           = flag.String("ip", "127.0.0.1", "ip")
+	port         = flag.Int("port", 10000, "port")
+	dbName       = flag.String("db", "test", "db")
+	concurrent   = flag.Int("concurrent", 16, "concurrent for insert")
+	batch        = flag.Int("batch", 64, "batch for insert")
+	shareFlag    = [4]bool{false}
+	timer        = time.NewTimer(1 * time.Second)
+	enableInsert = flag.Bool("insert", false, "enable_insert")
+	insertTime   = flag.Int("insert_time", 6, "insert_time hour")
+	dropTest     = flag.Bool("drop_test", true, "drop_test")
+	dropDelay    = flag.Int("drop_delay", 45, "drop_delay")
 )
 
 var (
@@ -47,30 +47,30 @@ var (
 func main() {
 	flag.Parse()
 
-	db := connect(*ip, *port, *db_name)
+	db := connect(*ip, *port, *dbName)
 
-	if *enable_insert {
+	if *enableInsert {
 		fmt.Println("Ensure you have run \"drop table t\"")
-		create_table(db)
-		insert_data(db)
-		timer.Reset(time.Duration(*insert_time) * time.Hour)
+		createTable(db)
+		insertData(db)
+		timer.Reset(time.Duration(*insertTime) * time.Hour)
 		<-timer.C
 		fmt.Println("Insert done")
 	}
 
-	if *drop_test {
+	if *dropTest {
 		*concurrent = 60
 		*batch = 1
-		shared_flag[0] = false
-		insert_data_job(db, 1)
+		shareFlag[0] = false
+		insertJob(db, 1)
 
-		fmt.Printf("waiting to drop... (%dmin)\n", *drop_delay)
-		timer.Reset(time.Duration(*drop_delay) * time.Minute)
+		fmt.Printf("waiting to drop... (%dmin)\n", *dropDelay)
+		timer.Reset(time.Duration(*dropDelay) * time.Minute)
 		<-timer.C
 		fmt.Println("start to drop", time.Now())
-		drop_partition(db, 0)
-		drop_partition(db, 2)
-		drop_partition(db, 3)
+		dropPartition(db, 0)
+		dropPartition(db, 2)
+		dropPartition(db, 3)
 		timer.Reset(40 * time.Minute)
 		<-timer.C
 		fmt.Println("All tests done")
@@ -87,7 +87,7 @@ func connect(ip string, port int, db string) *sql.DB {
 	return dbConn
 }
 
-func create_table(db *sql.DB) {
+func createTable(db *sql.DB) {
 	rand.Seed(time.Now().Unix())
 	_, err := db.Exec(sql1)
 	if err != nil {
@@ -95,7 +95,7 @@ func create_table(db *sql.DB) {
 	}
 }
 
-func drop_partition(db *sql.DB, num int) {
+func dropPartition(db *sql.DB, num int) {
 	sql := sql3 + fmt.Sprintf("p%v", num)
 	_, err := db.Exec(sql)
 	if err != nil {
@@ -103,23 +103,23 @@ func drop_partition(db *sql.DB, num int) {
 	}
 }
 
-func stop_all(db *sql.DB) {
+func stopAll(db *sql.DB) {
 	for i := 0; i < 4; i++ {
-		shared_flag[i] = true
+		shareFlag[i] = true
 	}
 }
 
-func insert_data(db *sql.DB) {
-	insert_data_job(db, 0)
-	insert_data_job(db, 1)
-	insert_data_job(db, 2)
-	insert_data_job(db, 3)
+func insertData(db *sql.DB) {
+	insertJob(db, 0)
+	insertJob(db, 1)
+	insertJob(db, 2)
+	insertJob(db, 3)
 	fmt.Println("All insert job started...")
 }
 
-func insert_data_job(db *sql.DB, part_num int) {
-	fmt.Println("Insert job to", part_num)
-	local_batch := *batch
+func insertJob(db *sql.DB, partNum int) {
+	fmt.Println("Insert job to", partNum)
+	local := *batch
 	for i := 0; i < *concurrent; i++ {
 		conn, err := db.Conn(context.Background())
 		if err != nil {
@@ -128,21 +128,21 @@ func insert_data_job(db *sql.DB, part_num int) {
 		go func() {
 			str := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+"
 			for {
-				id := part_num*5000 + rand.Intn(5000)
-				insert_sql := sql2
-				for i := 0; i < local_batch; i++ {
-					if i == local_batch-1 {
-						insert_sql += fmt.Sprintf(" (%v, '%v', '%v', '%v', '%v', '%v')", id+i, str, str, str, str, str)
+				id := partNum*5000 + rand.Intn(5000)
+				sql := sql2
+				for i := 0; i < local; i++ {
+					if i == local-1 {
+						sql += fmt.Sprintf(" (%v, '%v', '%v', '%v', '%v', '%v')", id+i, str, str, str, str, str)
 						break
 					} else {
-						insert_sql += fmt.Sprintf(" (%v, '%v', '%v', '%v', '%v', '%v'),", id+i, str, str, str, str, str)
+						sql += fmt.Sprintf(" (%v, '%v', '%v', '%v', '%v', '%v'),", id+i, str, str, str, str, str)
 					}
 				}
 				_, err := conn.ExecContext(context.Background(), insert_sql)
 				if err != nil {
 					fmt.Println(err)
 				}
-				if shared_flag[part_num] {
+				if shareFlag[partNum] {
 					break
 				}
 			}
